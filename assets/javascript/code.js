fa818e3caf0e59660a67f5819f6491c1bd159faa
Tracy Le <tnle9@asu.edu>
2019-01-31 18:18:48 -0800

    updated js

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,20 +1,24 @@
+
 // Build API request url for Yelp
 var corsProxy = "https://cors-anywhere.herokuapp.com/";
 var endpointUrl = "https://api.yelp.com/v3/businesses/search";
 var fullUrl = corsProxy + endpointUrl;
-
 // API key
 var apiKey = 'A6gmtllBxdAgu_ikQkG56eG9de48BNQVoLuwbxlnVlA-qySFfFokZCkAipK930RvAJF3NdjErxOOTVb2FoIvvW-_3NMsQyPhE6kpOUlvy8TVaUDw-VNvVFVMRlZBXHYx';
-
 // GET parameters to include
 var params = {
     term: "food",
     location: "San Diego",
     limit: 10
 };
-
+// var yelpName = ""
+// var yelpContact = {
+//     phone: "",
+//     address:""
+// };
+// var yelpRating = "";
+// var yelpImageUrl = "";
 var userInput = "";
-
 // Make API request for Yelp
 function yelpSearch(){
     $("#yelpCards").empty();
@@ -46,24 +50,18 @@ function createYelpCard(businessInfo) {
         }
     var yelpRating = businessInfo.rating;
     var yelpImageUrl = businessInfo.image_url;
-
     var card = $("<div>");
     card.addClass("card");
-
     var cardHeader = $("<div>");
     cardHeader.addClass("card-header");
-
     var cardBody = $("<div>");
     cardBody.addClass("card-body");
-
     var nameDiv = $("<div>");
     nameDiv.addClass("aaa");
     nameDiv.text(yelpName);
-
     var phoneDiv = $("<div>");
     phoneDiv.addClass("aaa");
     phoneDiv.text("Phone: " + yelpContact.phone);
-
     var addressDiv = $("<div>");
     addressDiv.addClass("aaa");
     addressDiv.append("Address: ");
@@ -71,31 +69,25 @@ function createYelpCard(businessInfo) {
     addressDiv.append(yelpContact.address.display_address[0]);
     addressDiv.append("<br>");
     addressDiv.append(yelpContact.address.display_address[1]);
-
     var ratingDiv = $("<div>");
     ratingDiv.addClass("aaa");
     ratingDiv.text("Rating: " + yelpRating);
-
     var imgDiv = $("<div>");
     imgDiv.addClass("aaa");
     var img = $("<img>");
     img.addClass("aaa");
     img.attr('src', yelpImageUrl);
     
+    
     imgDiv.append(img);
-
     cardHeader.append(nameDiv);
-
     cardBody.append(phoneDiv);
     cardBody.append(addressDiv);
     cardBody.append(ratingDiv);
     cardBody.append(imgDiv);
-
     card.append(cardHeader);
     card.append(cardBody);
-
     // $("#yelpCards").append(cardYelp);
-
     var cardYelp =
         '<div class="card">' +
             '<div class="card-header">' +
@@ -114,79 +106,18 @@ function createYelpCard(businessInfo) {
                 '</div>' +
             '</div>' +
         '</div>';
-
     // $("#yelpCards").append(cardYelp);
     document.getElementById("yelpCards").innerHTML += cardYelp;
     
 }
-function createRecipeCard () {
-    var card = $("<div>");
-    card.addClass("card");
-
-    var cardHeader = $("<div>");
-    cardHeader.addClass("card-header");
-
-    var cardBody = $("<div>");
-    cardBody.addClass("card-body");
-
-    var nameDiv = $("<div>");
-    nameDiv.addClass("aaa");
-    nameDiv.text(dogTitle);
-
-    var ingredientDiv = $("<div>");
-    ingredientDiv.addClass("aaa");
-    ingredientDiv.text("Ingredients: " + ingredients);
-
-    var imgDiv = $("<div>");
-    imgDiv.addClass("aaa");
-    var img = $("<img>");
-    img.addClass("aaa");
-    img.attr('src', dogImage);
-    
-    imgDiv.append(img);
-
-    cardHeader.append(nameDiv);
-
-    cardBody.append(ingredientDiv);
-    cardBody.append(imgDiv);
-
-    card.append(cardHeader);
-    card.append(cardBody);
-
-
-
-    var cardRecipe =
-        '<div class="card">' +
-            '<div class="card-header">' +
-                '<div class="aaa">Recipe: ' + dogTitle + '</div>' +
-            '</div>' +
-            '<div class="card-body">' +
-                '<div class="aaa">Ingredients: ' + ingredients + '</div>' +
-                '<div class="aaa">' + dogImage + '</div>' +
-            '</div>' +
-        '</div>';
-
-    // $("#yelpCards").append(card2);
-    document.getElementById("recipeCards").innerHTML += cardRecipe;
-}
-
-
 // API for Recipe Puppy
-
 function recipeSearch(event){
     $("#food-input").empty();
         
     var dogURl = "http://www.recipepuppy.com/api/?q=";
     console.log(userInput);
-
     //Complete URl with parameter
     var dogFullUrl = corsProxy + dogURl + userInput;
-    
-    var dogTitle = "" ;
-    var ingredients = "" ;
-    var link = "" ;
-    var dogImage = "";
-
     //Make ajax call
     $.ajax({
         url: dogFullUrl,
@@ -194,28 +125,69 @@ function recipeSearch(event){
     }).then(function(response){
         var data = JSON.parse(response);
         console.log(data);
-        for (i = 0; i < data.response.results; i++) {
-            dogTitle = data.response.results[i].title;
-            ingredients = data.response.results[i].ingredients;
-            link = data.response.results[i].href;
-            dogImage = data.response.results[i].thumbnail;
-            createRecipeCard(data.response.results[i]);
-        
-            console.log(dogTitle);
-            console.log(ingredients);
-            console.log(link);
-            console.log(dogImage);
-            $("#searchResults").show();
-            $("#recipeResults").show();
-        }
-
+        for (var i = 0; i < data.results.length; i++) {
+        createRecipeCard(data.results[i])
+        $("#searchResults").show();
+        $("#recipeResults").show();
+     console.log("hi");
+    } 
     })
-
+ function createRecipeCard(data) {
+    var dogImage = data.thumbnail;
+    var dogTitle = data.dogTitle;
+    var dogIngredients = data.ingredients
+    var dogLink = data.href;
+    console.log(dogImage);
+    console.log(dogTitle);
+    console.log(dogIngredients);
+    console.log(dogLink);
+    var card = $("<div>");
+    card.addClass("card");
+    var cardHeader = $("<div>");
+    cardHeader.addClass("card-header");
+    var cardBody = $("<div>");
+    cardBody.addClass("card-body");
+    var nameDiv = $("<div>");
+    nameDiv.addClass("bbb");
+    nameDiv.text(dogTitle);
     
+    var linkDiv = $("<div>");
+    linkDiv.addClass("bbb");
+    linkDiv.text("link:" + dogLink);
+    var ingDiv = $("<div>");
+    ingDiv.addClass("bbb");
+    linkDiv.text(dogIngredients);
+    
+    var dogImgDiv = $("<img>");
+    dogImgDiv.addClass("bbb");
+    dogImgDiv.attr("src",dogImage);
+    dogImgDiv.append(dogImage);
+    cardHeader.append(dogTitle);
+    linkDiv.append(dogLink);
+ 
+  
+    cardBody.append(dogImgDiv);
+    card.append(cardHeader);
+    card.append(cardBody);
+    var cardRecipe =
+        '<div class="card">' +
+            '<div class="card-header">' +
+                '<div class="bbb">recipe: ' + dogTitle + '</div>' +
+            '</div>' +
+            '<div class="card-body">' +
+                '<div class="bbb">Ingredients: ' + dogIngredients + '</div>' +
+                '<div class="bbb">' + dogImgDiv.html() + '</div>' +
+            '</div>' +
+        '</div>';
+    // $("#yelpCards").append(card2);
+    document.getElementById("recipeCards").innerHTML += cardRecipe;
+    console.log("end hi");
 }
-
+}
 $(document).ready(function(){
-
+    // Welcome page -show
+    // Click enter- hides welcome page
+    // Form page -shows
     $(".welcomeDescription").show();
     $('#formSearch').hide();
     $("#searchResults").hide();    
@@ -229,7 +201,6 @@ $(document).ready(function(){
             
         
     });
-
     $("#submitSearch").on("click", function(){
         userInput = $("#food-input").val().trim(); 
         $("#restaurantResults").hide();
@@ -238,21 +209,16 @@ $(document).ready(function(){
         var formSearch = document.getElementById("formSearch");
         var restaurantCheckbox = formSearch.restaurantCheckbox;
         var recipeCheckbox = formSearch.recipeCheckbox;
-
         
-
         if(restaurantCheckbox.checked === true) {
             // Call ajax yelpSearch function 
             yelpSearch();
-
             
             
         }
         if(recipeCheckbox.checked === true) {
             recipeSearch();
         }
-
     });
     
-    
-})
+    });
